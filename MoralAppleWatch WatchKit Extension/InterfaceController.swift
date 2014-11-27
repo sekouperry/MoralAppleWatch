86962715df31da97813d41b38d9b0af4c5595953
Alberto <alberto.moral.g@gmail.com>
2014-11-27 16:47:24 +0100

    Chapter 4 finished

---
+++ MoralAppleWatch WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,59 @@
+//
+//  InterfaceController.swift
+//  MoralAppleWatch WatchKit Extension
+//
+//  Created by Moral on 22/11/14.
+//  Copyright (c) 2014 Moral. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    @IBOutlet weak var moodImage: WKInterfaceImage!
+    var currentMood : Int = 0
+    override init(context: AnyObject?) {
+        // Initialize variables here.
+        super.init(context: context)
+        
+        // Configure interface objects here.
+        NSLog("%@ init", self)
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+        NSLog("%@ will activate", self)
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        NSLog("%@ did deactivate", self)
+        super.didDeactivate()
+    }
+
+    func moodSliderChanged(value: Float) {
+        
+        currentMood = Int(value)
+        println("Current mood : \(currentMood)")
+        
+        updateMoodImage()
+    }
+    
+    @IBAction func recordMood() {
+            pushControllerWithName("MoodDetailsController", context: nil)
+        
+    }
+    
+    
+    func updateMoodImage(){
+    
+        if currentMood < 0 || currentMood > 4 {
+            return
+        }
+        
+        moodImage.setImageNamed("rage\(currentMood)")
+    }
+}
---
+++ MoralAppleWatch WatchKit Extension/MoodDetailsController.swift
@@ -0,0 +1,18 @@
+//
+//  MoodDetailsController.swift
+//  MoralAppleWatch
+//
+//  Created by Moral on 27/11/14.
+//  Copyright (c) 2014 Moral. All rights reserved.
+//
+
+import Foundation
+import WatchKit
+
+class MoodDetailsController: WKInterfaceController {
+
+    @IBOutlet weak var moodAdviceLabel: WKInterfaceLabel!
+    @IBOutlet weak var moodImage: WKInterfaceImage!
+    @IBOutlet weak var moodNameLabel: WKInterfaceLabel!
+    @IBOutlet weak var moodDescriptionLabel: WKInterfaceLabel!
+}
